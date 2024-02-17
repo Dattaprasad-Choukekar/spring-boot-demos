554ce77b6002c5d45afffa6169f759a777416960
Datta <dattaprasad09@gmail.com>
2024-02-17 19:57:36 +0100

    Enable actuator

---
+++ spring-boot-actuator-demo/src/main/java/com/dattaprasad/boot/MyHealthIndicator.java
@@ -0,0 +1,14 @@
+package com.dattaprasad.boot;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        return Health.down(new NegativeArraySizeException()).build();
+        //return Health.unknown().build();
+    }
+}
