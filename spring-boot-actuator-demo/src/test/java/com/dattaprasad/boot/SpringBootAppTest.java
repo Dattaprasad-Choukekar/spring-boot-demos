bcbb0aeb92f33312370d906fb360067b53c7f70e
Madhuri <madhurisandbhor@gmail.com>
2024-02-17 11:28:12 +0100

    Init repository and setup spring-boot-actuator-demo

---
+++ spring-boot-actuator-demo/src/test/java/com/dattaprasad/boot/SpringBootAppTest.java
@@ -0,0 +1,12 @@
+package com.dattaprasad.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootAppTest {
+    @Test
+    public void contextLoads() {
+
+    }
+}
